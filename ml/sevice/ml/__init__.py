e80dc8b9b05b56e8667b17bf2941cf0e547cf19d
daawonn <58808398+daawonn@users.noreply.github.com>
2020-01-17 16:05:22 +0900

    번역 - 파파고를 이용한 모듈 만들기

--- ml/sevice/ml/__init__.py
+++ ml/sevice/ml/__init__.py
@@ -1,5 +1,10 @@
 from sklearn.externals import joblib 
 import json
+import re
+import os
+import sys
+import urllib.request
+
 
 # 1. python 3.3 이하 버전과 하위 호환을 위해서 사용
 # 2. 패키지 자체를 지칭할때 사용된다
@@ -9,22 +14,76 @@ def a():
     print('구동')
 
 # 0. 경로 -> 상수값 -> 환경변수 혹은 DB 에서 획득
-MODEL_PATH  = './ml_dl_projects/ml/sevice/ml/clf_model_202001161419.model'
-MODEL_LABEL = './ml_dl_projects/ml/sevice/ml/clf_labels.label'
+MODEL_PATH  = './ml/sevice/ml/clf_model_202001161419.model'
+MODEL_LABEL = './ml/sevice/ml/clf_labels.label'
 
-# 1. 모델 로드 (1회만) -> 요청이 많아지면 컨트롤이 가능한지 체크
-clf = joblib.load(MODEL_PATH)
-# 2. 레이블 로드
-with open (MODEL_LABEL,'r') as f:
-    clf_label = json.load(f)
+if __name__ != '__main__':
+    # 1. 모델 로드 (1회만) -> 요청이 많아지면 컨트롤이 가능한지 체크
+    clf = joblib.load(MODEL_PATH)
+
+    # 2. 레이블 로드
+    with open (MODEL_LABEL,'r') as f:
+        clf_label = json.load(f)
+
 # 3. 예측 함수 (input -> text , out -> 예측결과 ex> en,fr,....)
 
+def detect_lang( text ):
+
+    # 개인별로 신청한 lv2 open api 사용
+
+    # A. text => 빈도계산 => 알고리즘에 예측요청(데이터 주입) => 그 결과를 리턴
+    text = text.lower()                #1. 소문자처리
+    p    = re.compile('[^a-z]')        #2. 정규식(알파벳소문자만제외)
+    text = p.sub( '' , text )          #3. 소문자만 남는다.
+    counts = [ 0 for n in range(26) ]  # 알파벳별 카운트를 담을 공간(리스트)
+    limit_a = ord('a')                 # 반복해서 작업하니까 그냥 최초 한번 변수로 받아서 사용
+    for word in text:
+        counts[ord(word)-limit_a] += 1     # 문자 1개당 카운트 추가
+    total_count = sum(counts)
+    freq = list( map( lambda x:x/total_count , counts ) )        # 정규화
+
+    # B. 알고리즘에 예측요청(데이터 주입)
+    predict = clf.predict( [freq] )  # 입력 형태를 개발했던 형태와 동일하게 차원을 맞춘다.
+    na_code = predict[0]             # en, fr, ....
+    na_str = clf_label[na_code]      #'영어', '프랑스어', .... 
+
+
+    # C. 결과를 리턴
+    return na_code, na_str
+
+CLIENT_ID = 'vDFc8ZDZ6txFA2fxJUDa'
+SECRET_KEY = 'cLw_4HLzFP'
+PAPAGO_URL = "https://openapi.naver.com/v1/papago/n2mt"           #주소
+'''
+curl "https://openapi.naver.com/v1/papago/n2mt" \
+-H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
+-H "X-Naver-Client-Id: vDFc8ZDZ6txFA2fxJUDa" \
+-H "X-Naver-Client-Secret: cLw_4HLzFP" \
+-d "source=ko&target=en&text=만나서 반갑습니다." -v
+'''    
+
 # 4. 번역 함수 (현: 파파고 연동, 향후: RNN로 구현)    
+def transfer_lang( text, na_input_code = 'en', na_output_code = 'ko'):
+    print('파파고와 연동한 번역 처리 시작')
+    print(text)
+    encText = urllib.parse.quote(text)                    #한글의 url 인코딩처리 => %2D.... 변환처리
+    data    = "source={}&target={}&text={}".format(na_input_code,na_output_code,encText )  #파라미터 구성    
+    request = urllib.request.Request(PAPAGO_URL)            # 요청객체 생성
+    request.add_header("X-Naver-Client-Id",CLIENT_ID)       # 헤더설정
+    request.add_header("X-Naver-Client-Secret",SECRET_KEY)  # 헤더설정
+
+    response = urllib.request.urlopen(request, data=data.encode("utf-8")) # 요청
+    rescode = response.getcode()
+    if(rescode==200):  #응답성공
+        return json.load( response )
+    else:
+        return {}
 
 
 # 이 코드는 개발시 테스트 했던 코드이다.
 # 의도(개발시) 될때만 작동해야 한다.
 if __name__ == '__main__':
-    print('테스트', PI2)
-    a()
+    #print('테스트', PI2)
+    test_str = 'Bong Joon-ho (Korean: 봉준호, Korean pronunciation: [poːŋ tɕuːnho → poːŋdʑunɦo]; born September 14, 1969) is a South Korean film director and screenwriter. '
+    transfer_lang( test_str )
 
--- ml/sevice/run.py
+++ ml/sevice/run.py
@@ -1,8 +1,14 @@
 # 엔트리 포인트 : 진입로, 시작점
+
 # 1. 모듈 가져오기
+# 플라스크 관련 모듈 가져오기
 from flask import Flask, render_template, request, jsonify, redirect #첫글자 대문자 => 클래스
+# 테스트 모듈 가져오기
 from ml.mod import PI
-from ml import PI2
+# 언어감지 및 번역 모듈 가져오기
+from ml import detect_lang as dl, transfer_lang
+
+#---------------------------------------------------------------------#
 
 # 2. Flask 객체 생성
 app = Flask( __name__ )
@@ -10,12 +16,98 @@ app = Flask( __name__ )
 # 3. 라우팅
 @app.route('/')
 def home():
-    print(PI)
-    print(PI2)
+    #     text_str = '''
+        #     En 2000, Bong Joon-ho sort son premier long métrage Barking Dog. En 2003, il réalise Memories of Murder, un film tiré de l'histoire réelle d'un tueur en série. Le film est un grand succès et attire plus de 5 000 000 de spectateurs en Corée du Sud.
+
+        # The Host, son troisième long-métrage, est inspiré par un incident survenu en Corée du Sud à la fin des années 1980. Les Cahiers du cinéma le classent troisième film le plus important de 2006.
+
+        # En 2008, il se joint aux réalisateurs Leos Carax et Michel Gondry dans le film Tokyo! en proposant son court-métrage Shaking Tokyo.
+
+        # En septembre 2009, il est membre du jury international présidé par Laurent Cantet lors du 57e Festival de Saint-Sébastien. Cette même année, il dévoile son quatrième long-métrage, le drame Mother, présenté en compétition au Festival de Cannes 2009.
+
+        # Il revient en 2013 avec une coproduction internationale : le thriller de science-fiction Snowpiercer, le Transperceneige, adaptation du classique de la bande dessinée française homonyme, Le Transperceneige. Joon-ho signe une nouvelle fois lui-même le scénario, et réunit une distribution majoritairement anglo-américaine, menée par Chris Evans, mais complétée par son acteur fétiche Song Kang-ho, qu'il avait déjà dirigé dans The Host, et la jeune Go Ah-seong. Le long-métrage marque sa seconde incursion dans une fiction de genre adulte et spectaculaire.
+
+        # En 2014, il est président du jury du 38e Festival international du film de Hong Kong. La même année il est membre du jury du 19e Festival de Busan, sous la présidence d'Asghar Farhadi.
+
+        # En février 2015, il fait partie des membres du jury des longs métrages lors du 65e Festival de Berlin, présidé par Darren Aronofsky. Cette même année, le Festival International du Film de Belfort Entrevues lui consacre une rétrospective1.
+
+        # L'année 2017 est marquée par la sortie sur Netflix du film de science-fiction Okja, qu'il identifie comme étant au croisement de The Host et Snowpiercer2. Cette fois, une distribution anglo-américaine — composée notamment de Paul Dano et Tilda Swinton — entoure une actrice principale coréenne.
+
+        # En mai 2019, son film Parasite, présenté au festival de Cannes 2019, remporte la Palme d'or à l'unanimité du Jury3
+        #     '''
+        #     print(dl(text_str))
+   
+
+    import os
+    import ml
+    import sys
+    import urllib.request
+     
+
+    client_id = ml.CLIENT_ID # 개발자센터에서 발급받은 Client ID 값
+    client_secret = ml.SECRET_KEY # 개발자센터에서 발급받은 Client Secret 값
+
+    encText = urllib.parse.quote("반갑습니다")                  #한글의 url 인코딩처리 => %2D....
+    data = "source=ko&target=en&text=" + encText               #파라미터 구성
+    url = "https://openapi.naver.com/v1/papago/n2mt"           #주소
+    request = urllib.request.Request(url)                      # 요청객체 생성
+    request.add_header("X-Naver-Client-Id",client_id)          #헤더설정
+    request.add_header("X-Naver-Client-Secret",client_secret)  #헤더설정
+
+    response = urllib.request.urlopen(request, data=data.encode("utf-8")) # 요청
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+    else:
+        print("Error Code:" + rescode)
+
     return render_template('index.html')
+
+@app.route('/bsgo', methods=['GET','POST'])
+def bsgo():
+    if request.method == 'GET':
+        return render_template('bsgo.html')
+    else :
+        # 전달 된 데이터 획득
+        # print(request.form.get('o'))
+        oriTxt = request.form.get('o')  # 내용이 들어있고 100글자 이상이다.
+        # print(request.form['o'])  # 만약 키가 틀리면 오류를 발생한다
+        # 언어감지
+        na_code, na_str = dl( oriTxt )
+        
+        if na_code:  # 예측 되었다.
+            res = {
+                'code':na_code,
+                'code_str':na_str
+            }
+
+        else:
+            res = {
+                'code':'0',
+                'code_str':'언어 감지 실패'
+            }
+        # 결과응답                
+        return jsonify( res )
+        # pass 
+
+@app.route('/transfer', methods=['POST'])
+def transfer():
+    # 데이터를 획득
+    oriTxt = request.form.get('o')
+    na     = request.form.get('na')
+    # 번역
+    print(oriTxt)
+    res    = transfer_lang( oriTxt, na )
+    # 로그처리
+
+    # 응답
+    return  jsonify( res )
+
 
 # 4. 서버가동
 if __name__ == '__main__' :
     app.run(debug=True)
 else:
-    print('작동 X')    
+    print('작동 X')
+
